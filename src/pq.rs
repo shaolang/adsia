a61ab1f4d8460be470a3d9dc58941ed4bacaf1ec
shaolang <shaolang@ymail.com>
2020-05-20 16:30:43 +0800

    Implement pq.top and fix pq.highest_priority_child

--- src/pq.rs
+++ src/pq.rs
@@ -15,12 +15,25 @@ impl <T> Heap<T> {
         Heap { num_child, pairs: Vec::new() }
     }
 
-    pub fn top(&mut self) -> T {
-        todo!();
+    pub fn top(&mut self) -> Option<T> {
+        match self.pairs.len() {
+            0 => None,
+            1 => Some(self.pairs.pop().unwrap().item),
+            n => {
+                self.pairs.swap(0, n - 1);
+                let item = self.pairs.pop().unwrap().item;
+                self.push_down(0);
+                Some(item)
+            }
+        }
     }
 
-    pub fn peek(&self) -> &T {
-        &self.pairs.get(0).unwrap().item
+    pub fn peek(&self) -> Option<&T> {
+        if self.pairs.is_empty() {
+            None
+        } else {
+            Some(&self.pairs.get(0).unwrap().item)
+        }
     }
 
     pub fn insert(&mut self, item: T, priority: i8) {
@@ -72,14 +85,24 @@ impl <T> Heap<T> {
     }
 
     fn first_leaf_index(&self) -> usize {
-        todo!();
+        if self.pairs.len() < 2 {
+            0
+        } else {
+            (self.pairs.len() - 2) / self.num_child + 1
+        }
     }
 
     fn highest_priority_child(&self, index: usize) -> usize {
-        let indices = (1..self.num_child + 1)
+        let indices = (0..self.num_child)
             .map(|i| self.num_child * index + i)
             .collect::<Vec<usize>>();
-        *indices.iter().max().unwrap()
+        *indices.iter()
+            .fold(&indices[0], |acc, x|
+                  if self.pairs[*acc].priority > self.pairs[*x].priority {
+                      acc
+                  } else {
+                      x
+                  })
     }
 }
 
@@ -114,7 +137,7 @@ mod tests {
         h.insert("hello".to_string(), 10);
         h.insert("world".to_string(), 9);
 
-        assert_eq!(h.peek(), "hello");
+        assert_eq!(h.peek().unwrap(), "hello");
     }
 
     #[test]
@@ -123,7 +146,7 @@ mod tests {
         h.insert("hello".to_string(), 9);
         h.insert("world".to_string(), 10);
 
-        assert_eq!(h.peek(), "world");
+        assert_eq!(h.peek().unwrap(), "world");
     }
 
     #[test]
@@ -133,6 +156,35 @@ mod tests {
         h.insert("world".to_string(), 8);
         h.insert("universe".to_string(), 9);
 
-        assert_eq!(h.peek(), "hello");
+        assert_eq!(h.peek().unwrap(), "hello");
+    }
+
+    #[test]
+    fn top_removes_first_item_in_heap() {
+        let mut h: Heap<i8> = Heap::new(2);
+        h.insert(100, 10);
+        h.insert(90, 9);
+
+        h.top().unwrap();
+        assert_eq!(h.peek().unwrap(), &90);
+    }
+
+    #[test]
+    fn top_ensures_highest_priority_child_gets_bubbled_up() {
+        let mut h: Heap<i8> = Heap::new(3);
+        h.insert(100, 10);
+        h.insert(80, 8);
+        h.insert(40, 4);
+        h.insert(70, 7);
+
+        h.insert(60, 6);
+        h.insert(50, 5);
+        h.insert(90, 9);
+
+        h.insert(30, 3);
+
+        h.top().unwrap();
+        println!("{:?}", h);
+        assert_eq!(h.peek().unwrap(), &90);
     }
 }
